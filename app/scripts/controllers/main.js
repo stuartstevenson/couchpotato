5b290a75c7d6251c6c074d4a5e5e1f01671733b6
stuart stevenson <stuart.stevenson@rightmove.co.uk>
2014-07-28 13:52:30 +0100

    Now loops over tv urls with the correct timeout

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,14 +8,31 @@
  * Controller of the couchPotatoApp
  */
 angular.module("couchPotatoApp")
-	.controller("MainCtrl", ["$scope", "$sce", "LocalTvConfig", "TvConfig", function ($scope, $sce, LocalTvConfig, TvConfig) {
+	.controller("MainCtrl", ["$scope", "$sce", "$timeout", "LocalTvConfig", "TvConfig", function ($scope, $sce, $timeout, LocalTvConfig, TvConfig) {
 		var localTvConfigId = LocalTvConfig.getLocalTvConfigId();
 		if(localTvConfigId){
 			TvConfig.get({id: localTvConfigId}, function(tvConfig){
+
+                var currentPosition = 0;
+                var timerPromise;
+
+                function moveOnAndWait(){
+                    if(currentPosition + 1 > tvConfig.urls.length - 1){
+                        currentPosition = 0;
+                    }
+                    else{
+                        currentPosition += 1;
+                    }
+                    $scope.currentUrl = tvConfig.urls[currentPosition].url;
+                    timerPromise = $timeout(moveOnAndWait, tvConfig.urls[currentPosition].interval * 1000);
+                }
+
 				angular.forEach(tvConfig.urls, function(key){
 					key.url = $sce.trustAsResourceUrl(key.url);
 				});
-				$scope.tvConfig = tvConfig;
+
+				$scope.currentUrl = tvConfig.urls[0].url;
+                timerPromise = $timeout(moveOnAndWait, tvConfig.urls[0].interval * 1000)
 			});
 		}
 	}]);
