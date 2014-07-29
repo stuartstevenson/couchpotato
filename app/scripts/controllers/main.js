6f8b9a9b6ad7fe3c377beb1cb915c69b81eb082f
James Pace <james.a.pace@gmail.com>
2014-07-29 22:04:44 +0100

    The poller now works

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,31 +8,48 @@
  * Controller of the couchPotatoApp
  */
 angular.module("couchPotatoApp")
-	.controller("MainCtrl", ["$scope", "$sce", "$timeout", "LocalTvConfig", "TvConfig", function ($scope, $sce, $timeout, LocalTvConfig, TvConfig) {
+	.controller("MainCtrl", ["$scope", "$sce", "$timeout", "poller", "LocalTvConfig", "TvConfig", function ($scope, $sce, $timeout, poller, LocalTvConfig, TvConfig) {
 		var localTvConfigId = LocalTvConfig.getLocalTvConfigId();
-		if(localTvConfigId){
-			TvConfig.get({id: localTvConfigId}, function(tvConfig){
+		var currentPosition = 0;
+		var timerPromise;
+		var tvConfig;
 
-                var currentPosition = 0;
-                var timerPromise;
+		function moveOnAndWait() {
+			if (currentPosition + 1 > tvConfig.urls.length - 1) {
+				currentPosition = 0;
+			}
+			else {
+				currentPosition += 1;
+			}
+			$scope.currentScreen = tvConfig.urls[currentPosition];
+			$scope.currentScreen.url = $sce.trustAsResourceUrl($scope.currentScreen.url);
+			timerPromise = $timeout(moveOnAndWait, tvConfig.urls[currentPosition].interval * 1000);
+		}
 
-                function moveOnAndWait(){
-                    if(currentPosition + 1 > tvConfig.urls.length - 1){
-                        currentPosition = 0;
-                    }
-                    else{
-                        currentPosition += 1;
-                    }
-                    $scope.currentUrl = tvConfig.urls[currentPosition].url;
-                    timerPromise = $timeout(moveOnAndWait, tvConfig.urls[currentPosition].interval * 1000);
-                }
-
-				angular.forEach(tvConfig.urls, function(key){
-					key.url = $sce.trustAsResourceUrl(key.url);
-				});
-
-				$scope.currentUrl = tvConfig.urls[0].url;
-                timerPromise = $timeout(moveOnAndWait, tvConfig.urls[0].interval * 1000)
+		if (localTvConfigId) {
+			var tvConfigPoller = poller.get(TvConfig, {
+				action: "get",
+				params: {
+					id: localTvConfigId
+				}
 			});
+
+			tvConfigPoller.promise.then(
+				null,
+				null,
+				function (data) {
+					tvConfig = data;
+					if(!timerPromise) {
+						tvConfig.urls[0].url = $sce.trustAsResourceUrl(tvConfig.urls[0].url);
+						$scope.currentScreen = tvConfig.urls[0];
+						timerPromise = $timeout(moveOnAndWait, tvConfig.urls[currentPosition].interval * 1000);
+					}
+				}
+			);
+
+			$scope.$on('$destroy', function destroy() {
+				$timeout.cancel(timerPromise);
+				tvConfigPoller.stop();
+			})
 		}
 	}]);
